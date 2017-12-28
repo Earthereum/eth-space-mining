886cc8364e891f0005cb1a3e030a13c3c75a7cad
Austin Atchley <austinatchley@gmail.com>
2017-12-28 11:40:21 -0600

    Added contracts to migration, put ERC721 in SpaceOwnership

--- contracts/SpaceOwnership.sol
+++ contracts/SpaceOwnership.sol
@@ -1,7 +1,32 @@
 pragma solidity ^0.4.18;
 
 import "./SpaceBase.sol";
-import "./ERC721.sol";
+
+
+/// @title Interface for contracts conforming to ERC-721: Non-Fungible Tokens
+/// @author Dieter Shirley <dete@axiomzen.co> (https://github.com/dete)
+contract ERC721 {
+    // Required methods
+    function totalSupply() public view returns (uint256 total);
+    function balanceOf(address _owner) public view returns (uint256 balance);
+    function ownerOf(uint256 _tokenId) external view returns (address owner);
+    function approve(address _to, uint256 _tokenId) external;
+    function transfer(address _to, uint256 _tokenId) external;
+    function transferFrom(address _from, address _to, uint256 _tokenId) external;
+
+    // Events
+    event Transfer(address from, address to, uint256 tokenId);
+    event Approval(address owner, address approved, uint256 tokenId);
+
+    // Optional
+    // function name() public view returns (string name);
+    // function symbol() public view returns (string symbol);
+    // function tokensOfOwner(address _owner) external view returns (uint256[] tokenIds);
+    // function tokenMetadata(uint256 _tokenId, string _preferredTransport) public view returns (string infoUrl);
+
+    // ERC-165 Compatibility (https://github.com/ethereum/EIPs/issues/165)
+    function supportsInterface(bytes4 _interfaceID) external view returns (bool);
+}
 
 contract SpaceOwnership is SpaceBase, ERC721 {
 
@@ -11,15 +36,10 @@ contract SpaceOwnership is SpaceBase, ERC721 {
 
     // bool public implementsERC721 = true;
     //
-    function implementsERC721() public pure returns (bool)
+    function supportsInterface(bytes4 _interfaceID) external view returns (bool)
     {
-        return true;
+        return _interfaceID > 0;
     }
-    
-    // Internal utility functions: These functions all assume that their input arguments
-    // are valid. We leave it to public methods to sanitize their inputs and follow
-    // the required logic.
-
     /// @dev Checks if a given address is the current owner of a particular planet.
     /// @param _claimant the address we are validating against.
     /// @param _tokenId planet id, only valid when > 0
@@ -43,17 +63,6 @@ contract SpaceOwnership is SpaceBase, ERC721 {
         planetIndexToApproved[_tokenId] = _approved;
     }
 
-    /// @dev Transfers a planet owned by this contract to the specified address.
-    ///  Used to rescue lost planets. (There is no "proper" flow where this contract
-    ///  should be the owner of any planet. This function exists for us to reassign
-    ///  the ownership of planets that users may have accidentally sent to our address.)
-    /// @param _planetId - ID of planet
-    /// @param _recipient - Address to send the cat to
-    function rescueLostplanet(uint256 _planetId, address _recipient) public onlyCOO whenNotPaused {
-        require(_owns(this, _planetId));
-        _transfer(this, _recipient, _planetId);
-    }
-
     /// @notice Returns the number of planets owned by a specific address.
     /// @param _owner The owner address to check.
     /// @dev Required for ERC-721 compliance
@@ -71,12 +80,22 @@ contract SpaceOwnership is SpaceBase, ERC721 {
         address _to,
         uint256 _tokenId
     )
-        public
+        external
         whenNotPaused
     {
         // Safety check to prevent against an unexpected 0x0 default.
         require(_to != address(0));
-        // You can only send your own cat.
+        // Disallow transfers to this contract to prevent accidental misuse.
+        // The contract should never own any planets (except very briefly
+        // after a gen0 planet is created and before it goes on auction).
+        require(_to != address(this));
+        // Disallow transfers to the auction contracts to prevent accidental
+        // misuse. Auction contracts should only take ownership of planets
+        // through the allow + transferFrom flow.
+        // require(_to != address(saleAuction));
+        // require(_to != address(siringAuction));
+
+        // You can only send your own planet.
         require(_owns(msg.sender, _tokenId));
 
         // Reassign ownership, clear pending approvals, emit Transfer event.
@@ -93,7 +112,7 @@ contract SpaceOwnership is SpaceBase, ERC721 {
         address _to,
         uint256 _tokenId
     )
-        public
+        external
         whenNotPaused
     {
         // Only an owner can grant transfer approval.
@@ -118,9 +137,15 @@ contract SpaceOwnership is SpaceBase, ERC721 {
         address _to,
         uint256 _tokenId
     )
-        public
+        external
         whenNotPaused
     {
+        // Safety check to prevent against an unexpected 0x0 default.
+        require(_to != address(0));
+        // Disallow transfers to this contract to prevent accidental misuse.
+        // The contract should never own any planets (except very briefly
+        // after a gen0 planet is created and before it goes on auction).
+        require(_to != address(this));
         // Check for approval and valid ownership
         require(_approvedFor(msg.sender, _tokenId));
         require(_owns(_from, _tokenId));
@@ -138,7 +163,7 @@ contract SpaceOwnership is SpaceBase, ERC721 {
     /// @notice Returns the address currently assigned ownership of a given planet.
     /// @dev Required for ERC-721 compliance.
     function ownerOf(uint256 _tokenId)
-        public
+        external
         view
         returns (address owner)
     {
@@ -147,30 +172,75 @@ contract SpaceOwnership is SpaceBase, ERC721 {
         require(owner != address(0));
     }
 
-    /// @notice Returns the nth planet assigned to an address, with n specified by the
-    ///  _index argument.
+    /// @notice Returns a list of all planet IDs assigned to an address.
     /// @param _owner The owner whose planets we are interested in.
-    /// @param _index The zero-based index of the cat within the owner's list of cats.
-    ///  Must be less than balanceOf(_owner).
-    /// @dev This method MUST NEVER be called by smart contract code. It will almost
-    ///  certainly blow past the block gas limit once there are a large number of
-    ///  planets in existence. Exists only to allow off-chain queries of ownership.
-    ///  Optional method for ERC-721.
-    function tokensOfOwnerByIndex(address _owner, uint256 _index)
-        external
-        view
-        returns (uint256 tokenId)
-    {
-        uint256 count = 0;
-        for (uint256 i = 1; i <= totalSupply(); i++) {
-            if (planetIndexToOwner[i] == _owner) {
-                if (count == _index) {
-                    return i;
-                } else {
-                    count++;
+    /// @dev This method MUST NEVER be called by smart contract code. First, it's fairly
+    ///  expensive (it walks the entire planet array looking for Planets belonging to owner),
+    ///  but it also returns a dynamic array, which is only supported for web3 calls, and
+    ///  not contract-to-contract calls.
+    function tokensOfOwner(address _owner) external view returns(uint256[] ownerTokens) {
+        uint256 tokenCount = balanceOf(_owner);
+
+        if (tokenCount == 0) {
+            // Return an empty array
+            return new uint256[](0);
+        } else {
+            uint256[] memory result = new uint256[](tokenCount);
+            uint256 totalPlanets = totalSupply();
+            uint256 resultIndex = 0;
+
+            // We count on the fact that all Planets have IDs starting at 1 and increasing
+            // sequentially up to the totalplanet count.
+            uint256 planetId;
+
+            for (planetId = 1; planetId <= totalPlanets; planetId++) {
+                if (planetIndexToOwner[planetId] == _owner) {
+                    result[resultIndex] = planetId;
+                    resultIndex++;
                 }
             }
+
+            return result;
         }
-        revert();
+    }
+
+    /// @dev Adapted from memcpy() by @arachnid (Nick Johnson <arachnid@notdot.net>)
+    ///  This method is licenced under the Apache License.
+    ///  Ref: https://github.com/Arachnid/solidity-stringutils/blob/2f6ca9accb48ae14c66f1437ec50ed19a0616f78/strings.sol
+    function _memcpy(uint _dest, uint _src, uint _len) private pure {
+        // Copy word-length chunks while possible
+        for(; _len >= 32; _len -= 32) {
+            assembly {
+                mstore(_dest, mload(_src))
+            }
+            _dest += 32;
+            _src += 32;
+        }
+
+        // Copy remaining bytes
+        uint256 mask = 256 ** (32 - _len) - 1;
+        assembly {
+            let srcpart := and(mload(_src), not(mask))
+            let destpart := and(mload(_dest), mask)
+            mstore(_dest, or(destpart, srcpart))
+        }
+    }
+
+    /// @dev Adapted from toString(slice) by @arachnid (Nick Johnson <arachnid@notdot.net>)
+    ///  This method is licenced under the Apache License.
+    ///  Ref: https://github.com/Arachnid/solidity-stringutils/blob/2f6ca9accb48ae14c66f1437ec50ed19a0616f78/strings.sol
+    function _toString(bytes32[4] _rawBytes, uint256 _stringLength) private pure returns (string) {
+        var outputString = new string(_stringLength);
+        uint256 outputPtr;
+        uint256 bytesPtr;
+
+        assembly {
+            outputPtr := add(outputString, 32)
+            bytesPtr := _rawBytes
+        }
+
+        _memcpy(outputPtr, bytesPtr, _stringLength);
+
+        return outputString;
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 var Users = artifacts.require("./Users.sol");
+var SpaceBase = artifacts.require("./SpaceBase.sol");
+var SpaceAccessControl = artifacts.require("./SpaceAccessControl.sol");
+var SpaceOwnership = artifacts.require("./SpaceOwnership.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Users);
+  deployer.deploy(SpaceBase);
+  deployer.deploy(SpaceAccessControl);
+  deployer.deploy(SpaceOwnership);
 };
+
