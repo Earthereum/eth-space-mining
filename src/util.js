66be297d929a877e6e8131a61a6ce0cfd963ca17
Austin Atchley <austinatchley@gmail.com>
2018-01-12 16:16:55 -0600

    Commented genome computations, changed numTerrain range

--- src/util.js
+++ src/util.js
@@ -33,32 +33,39 @@ function computeSeed (genome) {
 }
 
 function computeSize (genome) {
+  // shift down 10 digits, get a value between 0 and 1
   let multiplier = genome.shift(10).modulo(0x10000).div(0x10000).toNumber();
+  // limit our sizes to 0.4 - 0.8
   return (multiplier * 0.4) + 0.4;
 }
 
 function computeWater (genome) {
+  // shift down 8 digits, get a value between 0 and 1
   return genome.shift(8).modulo(0x10000).div(0x10000).toNumber();
 }
 
 function computeAtmoDensity (genome) {
+  // shift down 6 digits, get a value between 0 and 1
   return genome.shift(6).modulo(0x10000).div(0x10000).toNumber();
 }
 
 function computeCloudDensity (genome) {
+  // shift down 4 digits, get a value between 0 and 1
   return genome.shift(4).modulo(0x10000).div(0x10000).toNumber();
 }
 
 function computeBaseColor (genome) {
+  // shift down 10 digits, get a 6 digit value for color
   return genome.shift(10).modulo(0x1000000).toNumber();
 }
 
 function computeAccColor (genome) {
+  // shift down 4 digits, get a 6 digit value for color
   return genome.shift(4).modulo(0x1000000).toNumber();
 }
 
 function computeNumTerrains (genome) {
-  return Math.ceil(Math.random() * 4);
+  return Math.ceil(genome.modulo(10));
 }
 
 function computePrice () {
