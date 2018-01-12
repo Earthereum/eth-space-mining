8077308833e5fc0dcabdf1310554218a527f0102
Austin Atchley <austinatchley@gmail.com>
2018-01-12 15:39:05 -0600

    Split computation of genome attributes into separate methods, QoL changes

--- src/util.js
+++ src/util.js
@@ -7,16 +7,16 @@ export async function processGenome (id) {
   return {
     title: 'Planet ' + id,
     traits: {
-      'seed': genome.modulo(0x10000).toNumber(),
-      'size': Math.random(),
-      'water': Math.random(),
-      'atmoDensity': Math.random(),
-      'cloudDensity': Math.random(),
-      'baseColor': genome.shift(10).modulo(0x1000000).toString(16),
-      'accColor': genome.shift(4).modulo(0x1000000).toString(16),
-      'numTerrains': Math.ceil(Math.random() * 4)
+      'seed': computeSeed(genome),
+      'size': computeSize(genome),
+      'water': computeWater(genome),
+      'atmoDensity': computeAtmoDensity(genome),
+      'cloudDensity': computeCloudDensity(genome),
+      'baseColor': computeBaseColor(genome),
+      'accColor': computeAccColor(genome),
+      'numTerrains': computeNumTerrains(genome)
     },
-    price: Math.random(),
+    price: computePrice(),
     link: '/planet/' + id,
     id: id
   }
@@ -26,4 +26,41 @@ async function getGenome (id) {
   const coreInstance = await window.contracts.Core.deployed();
   let planetResult = await coreInstance.getPlanet.call(id);
   return planetResult[5];
+}
+
+function computeSeed (genome) {
+  return genome.modulo(0x10000).toNumber();
+}
+
+function computeSize (genome) {
+  let multiplier = genome.shift(10).modulo(0x10000).div(0x10000).toNumber();
+  return (multiplier * 0.4) + 0.4;
+}
+
+function computeWater (genome) {
+  return genome.shift(8).modulo(0x10000).div(0x10000).toNumber();
+}
+
+function computeAtmoDensity (genome) {
+  return genome.shift(6).modulo(0x10000).div(0x10000).toNumber();
+}
+
+function computeCloudDensity (genome) {
+  return genome.shift(4).modulo(0x10000).div(0x10000).toNumber();
+}
+
+function computeBaseColor (genome) {
+  return genome.shift(10).modulo(0x1000000).toNumber();
+}
+
+function computeAccColor (genome) {
+  return genome.shift(4).modulo(0x1000000).toNumber();
+}
+
+function computeNumTerrains (genome) {
+  return Math.ceil(Math.random() * 4);
+}
+
+function computePrice () {
+  return Math.ceil(Math.random() * 10000) / 10000;
 }
