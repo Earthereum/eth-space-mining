ad71cc4f5944d264726f822fbc46067a34c91f8c
Logan Zartman <logan.zartman@utexas.edu>
2018-01-16 19:42:24 -0600

    Modularized auction button on planet detail page

---
+++ src/components/AuctionButton.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-dialog v-if="isOwnPlanet" v-model="sellDialog" persistent max-width="500px">
+    <v-btn slot="activator">
+      <v-icon left dark>account_balance</v-icon>
+      Sell
+    </v-btn>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Create Auction for {{ planet.name }}</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container grid-list-md>
+          <v-layout wrap>
+            <v-flex xs12 sm6>
+              <v-text-field
+                label="Price"
+                prefix="Ξ"
+                v-model="sellPrice"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <v-text-field
+                label="Duration"
+                v-model="sellDuration"
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="red" flat @click.native="sellDialog = false">Cancel</v-btn>
+        <v-btn flat @click.native="createAuction">Start</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  import BigNumber from 'bignumber.js';
+
+  export default {
+    props: ['planet'],
+    data: () => {
+      return {
+        isOwnPlanet: false,
+        sellDialog: false,
+        sellPrice: null,
+        sellDuration: null
+      }
+    },
+    async created () {
+      const currentUser = window.web3.eth.accounts[0];
+
+      try {
+        // see who owns it
+        const coreInstance = await window.contracts.Core.deployed();
+        const ownerData = await coreInstance.ownerOf.call(this.planet.id);
+        this.owner = new BigNumber(ownerData);
+        this.isOwnPlanet = this.owner.equals(currentUser);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    methods: {
+      async createAuction () {
+        const currentUser = window.web3.eth.accounts[0];
+        const core = await window.contracts.Core.deployed();
+
+        this.sellDialog = false;
+
+        // TODO: update
+        await core.createSaleAuction(
+          this.planet.id,
+          this.sellPrice,
+          this.sellPrice,
+          this.sellDuration,
+          {from: currentUser}
+        );
+      }
+    }
+  }
+</script>
--- src/components/Planet.vue
+++ src/components/Planet.vue
@@ -29,41 +29,7 @@
                               <v-icon left dark>account_balance</v-icon>
                               Buy Ξ{{ planet.price }}
                           </v-btn>
-                          <v-dialog v-if="isOwnPlanet" v-model="sellDialog" persistent max-width="500px">
-                            <v-btn slot="activator">
-                              <v-icon left dark>account_balance</v-icon>
-                              Sell
-                            </v-btn>
-                            <v-card>
-                              <v-card-title>
-                                <span class="headline">Create Auction</span>
-                              </v-card-title>
-                              <v-card-text>
-                                <v-container grid-list-md>
-                                  <v-layout wrap>
-                                    <v-flex xs12 sm6>
-                                      <v-text-field
-                                        label="Price"
-                                        prefix="Ξ"
-                                        v-model="sellPrice"
-                                      ></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12 sm6>
-                                      <v-text-field
-                                        label="Duration"
-                                        v-model="sellDuration"
-                                      ></v-text-field>
-                                    </v-flex>
-                                  </v-layout>
-                                </v-container>
-                              </v-card-text>
-                              <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="red" flat @click.native="sellDialog = false">Cancel</v-btn>
-                                <v-btn flat @click.native="createAuction">Start</v-btn>
-                              </v-card-actions>
-                            </v-card>
-                          </v-dialog>
+                          <auction-button :planet="planet"></auction-button>
                         </v-flex>
                         <v-flex xs12>
                           <h2>About {{ planet.title }}</h2>
@@ -82,7 +48,7 @@
 </template>
 
 <script>
-  import BigNumber from 'bignumber.js';
+  import AuctionButton from '@/components/AuctionButton';
   import {Planet} from 'earthereum-renderer';
   import {processGenome} from '../util.js';
   export default {
@@ -91,27 +57,15 @@
       return {
         planet: null,
         owner: null,
-        isOwnPlanet: false,
         forSale: false,
-        sellDialog: false,
-        sellPrice: null,
-        sellDuration: null,
         error: false
       };
     },
     async created () {
-      const currentUser = window.web3.eth.accounts[0];
-
       try {
         // lookup this planet
         const data = await processGenome(this.id);
         this.planet = data;
-
-        // see who owns it
-        const coreInstance = await window.contracts.Core.deployed();
-        const ownerData = await coreInstance.ownerOf.call(this.id);
-        this.owner = new BigNumber(ownerData);
-        this.isOwnPlanet = this.owner.equals(currentUser);
       } catch (e) {
         console.error(e);
         this.error = true;
@@ -120,23 +74,8 @@
     methods: {
       createPlanet (traits) {
         return new Planet(traits);
-      },
-
-      async createAuction () {
-        const currentUser = window.web3.eth.accounts[0];
-        const core = await window.contracts.Core.deployed();
-
-        this.sellDialog = false;
-
-        // TODO: update
-        await core.createSaleAuction(
-          this.id,
-          this.sellPrice,
-          this.sellPrice,
-          this.sellDuration,
-          {from: currentUser}
-        );
       }
-    }
+    },
+    components: {'auction-button': AuctionButton}
   }
 </script>
