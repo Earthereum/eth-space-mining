8f788aadf5aafa1689c52d673b48d14ae3472468
Austin Atchley <austinatchley@gmail.com>
2018-01-17 21:58:09 -0600

    Added extra async step to print correct SaleClockAuction address on deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -23,9 +23,9 @@ module.exports = function(deployer) {
   deployer.then(() => SpaceCore.deployed())
   .then(instance => coreInstance = instance)
   .then(() => {
-    coreInstance.setSaleAuctionAddress(SaleClockAuction.address);
+    return coreInstance.setSaleAuctionAddress(SaleClockAuction.address);
+  }).then(() => {
     console.log('Expected SaleClockAuction Address: ' + SaleClockAuction.address);
     coreInstance.saleAuction.call().then(val => console.log('Actual: ' + val));
-  })
-  .catch(err => console.error(err));
+  }).catch(err => console.error(err));
 };
